3048fe2e7e0ce85a022565edc325937619b87335
양탕구리 <rukeni@naver.com>
2022-04-28 16:29:52 +0900

    Modify : article 여백에서 스크롤 되도록 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react";
+import Comments from "./Comment";
 import Footer from "./Footer";
 import NavigationBar from "./Navigation/NavigationBar"
 import Sidebar from "./Sidebar/Sidebar"
@@ -12,13 +13,17 @@ const Layout: FC<LayoutProps> = ({children, title}) => {
       <NavigationBar />
       <div className="flex overflow-hidden h-screen">
       <Sidebar />
-      <article className='prose flex-1 m-auto mt-10 h-full overflow-auto'>
+      <article className='prose flex-1 m-auto w-[calc(100vw-16rem)] max-w-full px-48 mt-10 h-full overflow-auto'>
         {children}
       {/* 페이지 이동 */}
-
+      <div className="flex justify-between">
+       <button>이전</button>
+       <button>다음</button>
+      </div>
+      <Comments />
       </article>
       </div>
-      <Footer />
+      {/* <Footer /> */}
 
     </div>
   )
