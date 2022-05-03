4f0700707c8b8503e7286122bd9693fd584abe8f
rukeni <rukeni@naver.com>
2022-05-03 15:07:22 +0900

    Add: 네비게이션 메뉴 데이터 연동

--- components/Navigation/NavigationItem.tsx
+++ components/Navigation/NavigationItem.tsx
@@ -1,16 +1,15 @@
 import Link from 'next/link';
 
+import { navigationMenus } from './navigationMenus';
+
 const NavigationItem = () => {
   return (
     <ul className='flex ml-10'>
-      {new Array(10)
-        .fill(0)
-        .map((el, idx) => idx + 1)
-        .map((el, index) => (
-          <li className='mx-8' key={el}>
-            <Link href={`/data`}>{String(el)}</Link>
-          </li>
-        ))}
+      {navigationMenus.map((menu, index) => (
+        <li className='mx-8' key={menu.name}>
+          <Link href={menu.url}>{menu.name}</Link>
+        </li>
+      ))}
     </ul>
   );
 };
---
+++ components/Navigation/navigationMenus.ts
@@ -0,0 +1,22 @@
+export const navigationMenus = [
+  {
+    name: '회고록',
+    url: '/diary',
+  },
+  {
+    name: '리액트',
+    url: '/react',
+  },
+  {
+    name: '자바스크립트',
+    url: '/javascript',
+  },
+  {
+    name: 'CSS',
+    url: '/css',
+  },
+  {
+    name: '프론트엔드',
+    url: '/frontend',
+  },
+];
