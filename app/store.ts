e8706183f1304036c317d3c982a4c4b6f6ebc6f5
rukeni <rukeni@naver.com>
2022-05-03 15:25:41 +0900

    Add: redux-toolkit 기본 예제 추가

--- app/store.ts
+++ app/store.ts
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '@features/counter/counterSlice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 
-export const store = configureStore({
-  reducer: {},
-});
+export function makeStore() {
+  return configureStore({
+    reducer: { counter: counterReducer },
+  });
+}
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+const store = makeStore();
+
+export type AppState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action<string>
+>;
+
+export default store;
