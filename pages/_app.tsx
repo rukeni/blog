e28eebb3bc30f8c00d877026295e59055ec3c892
rukeni <rukeni@naver.com>
2022-05-03 15:25:29 +0900

    Chore: 절대경로 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 
+import Layout from '@components/Layout';
 import type { AppProps } from 'next/app';
-
-import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
