fd1b119537fad12e4c2cecd596132d7f1b8b5a44
rukeni <rukeni@naver.com>
2022-05-12 16:22:23 +0900

    Add: 예상 소요 시간 추가

--- components/Post/HeadPost.tsx
+++ components/Post/HeadPost.tsx
@@ -2,14 +2,21 @@ type HeadPostProps = {
   title: string;
   createdDate: string;
   updatedDate: string;
+  estimatedTime: string;
 };
-const HeadPost = ({ title, createdDate, updatedDate }: HeadPostProps) => {
+const HeadPost = ({
+  title,
+  createdDate,
+  updatedDate,
+  estimatedTime = '',
+}: HeadPostProps) => {
   return (
     <div>
       <h1 className='text-4xl font-bold text-center'>
         {title || '타이틀을 입력해주세요'}
       </h1>
       <div className='flex flex-col justify-start items-end'>
+        {estimatedTime && `예상 소요 시간: ${estimatedTime}분☕️`}
         <p className='my-0 lg:mt-0 lg:mb-1'>
           작성일: {createdDate || '입력필요'}
         </p>
